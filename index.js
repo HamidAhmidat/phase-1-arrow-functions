f9d232ca6bfb3df9d3d4f4273ecdd8e8f056e546
hamid <ahmidatabdelhamid@gmail.com>
2023-07-30 21:20:55 +0100

    Completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+// has a function expression called divide
+
+function divide(a, b) {
+
+    return (a / b);
+}
+// divide divides 2000 by 100
+console.log(divide("2000", "100"));
+
+
+// has an arrow function called square
+// square arrow function takes one parameter and multiplies it times itself
+const square = x => x * x;
+
+// has an arrow function called add
+// add arrow function takes two parameters and adds them together
+const add = (a, b) => (a + b);
+
+
